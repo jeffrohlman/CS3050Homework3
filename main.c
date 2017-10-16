ff5d4feb44dfb0cf953e519e9382b5163cc58a50
Jeffrey <Jeffrey@JeffreysPC>
2017-10-16 15:52:32 -0500

    First iteration of homework 1

--- main.c
+++ main.c
@@ -13,12 +13,83 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "input_error.h"
+#include "vector.h"
+#include "ctype.h"
 
-/*
- * 
- */
+void parse_getline(FILE*, struct vector adj[]);
+void parseline(char *, int *, int *);
+
+
+
 int main(int argc, char** argv) {
-
+    if(argc != 3)
+        exit(INCORRECT_NUMBER_OF_COMMAND_LINE_ARGUMENTS);
+    
+    FILE* fptr = fopen(*(argv + 1), "r");
+    if(!fptr)
+        exit(INPUT_FILE_FAILED_TO_OPEN);
+    
+    
+    if(fgetc(fptr) == EOF)
+        exit(PARSING_ERROR_EMPTY_INPUT_FILE);
+    fseek(fptr, 0, SEEK_SET);
+    
+    char *line = NULL;
+    size_t nbytes = 0;
+    getline(&line, &nbytes, fptr);
+    
+    int i = 0;
+    int num = 0;
+    int temp = 0;
+    while(line[i] != '\n'){
+        if(isdigit(line[i]) == 0)
+            exit(PARSING_ERROR_INVALID_FORMAT);
+        num *= 10;
+        num += line[i] - '0';
+        i++;
+    }
+    
+    printf("%d\n", num);
+    struct vector adjList[num];
+    for(i = 0; i < num; i++){
+        init_vector(&adjList[i]);
+    }
+    
+    parse_getline(fptr, adjList);
+    
     return (EXIT_SUCCESS);
 }
 
+//Function that scans file and pulls each line one at a time
+//Send individual lines to parseline to check for non digit characters
+//Adds to vector once determined to be an integer
+void parse_getline(FILE* fp, struct vector adj[]) {
+	char* line = NULL;
+	size_t nbytes = 0;
+        int linelen=0;
+        int* v1 = 0;
+        int* v2 = 0;
+        
+	while ((linelen=getline(&line, &nbytes, fp)) != -1) {
+		line[linelen-1] = '\0'; 
+		parseline(line, &v1, &v2);
+                insert_element_vector(&adj[*v1], *v2);
+                insert_element_vector(&adj[*v2], *v1);   
+	}
+
+	free(line);
+}
+
+//Function to take in single line and parse for non digit characters
+//If the function encounters a non digit character the program is exited
+void parseline(char *line, int *v1, int *v2) {
+	char c;
+        int i=1;
+        if(line[0] != '(')
+
+	while ((c = *(line + i++)) != '\0') {
+		if (!isdigit(c))
+                    exit(PARSING_ERROR_INVALID_CHARACTER_ENCOUNTERED);
+	}
+}
